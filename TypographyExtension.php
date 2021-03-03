0c4c36262bd70c41e9865b1398c86932a1364435
Petr Parimucha <petr@pari.cz>
2021-03-03 09:47:11 +0100

    Forgot dump removed

--- TypographyExtension.php
+++ TypographyExtension.php
@@ -78,7 +78,6 @@ final class TypographyExtension extends AbstractExtension {
     if (file_exists($this->settings_file_path)) {
       $defaults = (array) Yaml::parse(file_get_contents($this->settings_file_path));
     }
-    dump($defaults);
     return $defaults;
   }
 }
